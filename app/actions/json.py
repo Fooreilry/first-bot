87d1fe256cce8a94c149e5b49a9bdb4de1258c35
Lyuba <len.ovo4356@gmail.com>
2024-09-14 23:24:17 +0300

    creating a keyboard with two buttons and displaying the entire to-do list

---
+++ app/actions/json.py
@@ -0,0 +1,59 @@
+#json
+list_household_chores = [
+    {
+        'id': 0,
+        'name': 'Мыть посуду',
+        'count_in_week': 7,
+        'date_start': '20.01.2001',
+        'date_end': '20.01.2001'
+    },
+    {
+        'id': 1,
+        'name': 'Убирать туалет Бони',
+        'count_in_week': 7,
+        'date_start': '20.01.2001',
+        'date_end': '20.01.2001'
+    },
+    {
+        'id': 2,
+        'name': 'Выкинуть мусор',
+        'count_in_week': 2,
+        'date_start': '20.01.2001',
+        'date_end': '20.01.2001'
+    },
+    {
+        'id': 3,
+        'name': 'Мыть унитаз',
+        'count_in_week': 1,
+        'date_start': '20.01.2001',
+        'date_end': '20.01.2001'
+    },
+    {
+        'id': 4,
+        'name': 'Стирка',
+        'count_in_week': 3,
+        'date_start': '20.01.2001',
+        'date_end': '20.01.2001'
+    },
+    {
+        'id': 5,
+        'name': 'Развесить вещи',
+        'count_in_week': 3,
+        'date_start': '20.01.2001',
+        'date_end': '20.01.2001'
+    },
+    {
+        'id': 6,
+        'name': 'Протерать пыль',
+        'count_in_week': 1,
+        'date_start': '20.01.2001',
+        'date_end': '20.01.2001'
+    },
+    {
+        'id': 7,
+        'name': 'Мыть полы',
+        'count_in_week': 1,
+        'date_start': '20.01.2001',
+        'date_end': '20.01.2001'
+    }   
+]
